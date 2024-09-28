eb3055d28c24c733c607bccf77109389809c4cfe
Kunal-Das964 <wastematerial12345@gmail.com>
2024-09-28 10:08:25 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use script"; // treat all JS code as newer version
+
+// alert("hello") //we are using nodeJS, not browser
+
+console.log( 3 
+    + 
+    3)//code readability should be high
+
+console.log("Kunal")
+
+let name="Kunal"
+let age=21
+let isLoggedIn = false
+
+
+// number=> 2 to power 53
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => still value is not defined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+ 
