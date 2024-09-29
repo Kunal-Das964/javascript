01686f77cda71a8e0e6f42816fce2c1531f47839
Kunal Das <wastematerial12345@gmail.com>
2024-09-29 09:43:55 +0000

    memory study done

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -30,16 +30,47 @@ let myFunction = function(){
 
 } // result=object
 
-console.log(typeof name);
-console.log(typeof isLoggedIn);
-console.log(typeof todaysTemp);
-console.log(typeof userEmail);
-console.log(typeof id);
-console.log(typeof bigNumber);
-console.log(typeof heros);
-console.log(typeof myObj);
-console.log(typeof myFunction);
+// console.log(typeof name);
+// console.log(typeof isLoggedIn);
+// console.log(typeof todaysTemp);
+// console.log(typeof userEmail);
+// console.log(typeof id);
+// console.log(typeof bigNumber);
+// console.log(typeof heros);
+// console.log(typeof myObj);
+// console.log(typeof myFunction);
 
 
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// ******************************* Memory **************************************//
+
+//there are two type of memory 
+
+/*Stack:-1.it is used where primitive data types are used
+         2.when it is used only the copy of saved variable is available/change will occur in copy of the value
+*/
+
+/*Heap:-1.it is used where non primitive data types are used
+        2.when it is used the reference or the original of saved variable is available/change will occur in original value
+*/
+
+let myYoutubename = "kunaldasdotcom"
+let anothername = myYoutubename
+anothername = "Ajeetkumardotcom"
+
+console.log(myYoutubename);
+console.log(anothername); // stack memory allocation
+
+let user = {
+    email:"abc@google.com",
+    upi:"kunal@ybl"
+}
+let user1 =user;
+user1.upi = "ajydvusyxvsai@ybl"
+
+console.log(user);
+console.log(user1); // heap memory allocation
 
