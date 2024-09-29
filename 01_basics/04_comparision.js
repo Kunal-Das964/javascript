3fbd19d679a439d03a5022b14d4be066680b494e
Kunal-Das964 <wastematerial12345@gmail.com>
2024-09-29 08:07:53 +0000

    comparision done

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,13 +8,13 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0); //avoid these type of comparisions.
-console.log(null == 0); //avoid these type of comparisions.
-console.log(null >= 0); //avoid these type of comparisions.
+console.log(null > 0); //avoid these type of comparisions. It also give false
+console.log(null == 0); //avoid these type of comparisions. It also give false
+console.log(null >= 0); //avoid these type of comparisions. It also give true because here the comparision operator treat null as a 0
 
-console.log(undefined > 0); //avoid these type of comparisions.
-console.log(undefined == 0); //avoid these type of comparisions.
-console.log(undefined >= 0); //avoid these type of comparisions.
+console.log(undefined > 0); //avoid these type of comparisions. It also give false
+console.log(undefined == 0); //avoid these type of comparisions. It also give false
+console.log(undefined >= 0); //avoid these type of comparisions. It also give false
 
 
 
