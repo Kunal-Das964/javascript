3dbebe035394e12d2e822498174f4388cafc972f
Kunal-Das964 <wastematerial12345@gmail.com>
2024-09-29 08:05:51 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); //avoid these type of comparisions.
+console.log(null == 0); //avoid these type of comparisions.
+console.log(null >= 0); //avoid these type of comparisions.
+
+console.log(undefined > 0); //avoid these type of comparisions.
+console.log(undefined == 0); //avoid these type of comparisions.
+console.log(undefined >= 0); //avoid these type of comparisions.
+
+
+
+
