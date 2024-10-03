bb82ed0ab7c423c68ac4a988890a95123fa11c7a
Kunal Das <wastematerial12345@gmail.com>
2024-10-03 15:26:52 +0000

    more method of array done

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,29 @@
+// const marvel_heros = ["thor","Ironman","spiderman"]
+// const dc_heros = ["superman","flash","batman"]
+
+// marvel_heros.push(dc_heros) // this is a method of array which join marvel hero's array with dc hero's array but it take complete dc hero's array as a element of marvel hero's array
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);// when we find the element of array in array
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros); // it joins the array mervel and dc hero's array
+
+// const all_new_heros = [...marvel_heros,...dc_heros]
+// console.log(all_new_heros);// it also joins the two arrays
+
+// const another_array = [1,2,3,[4,5,6,[7,8,9]]]
+
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);// this method is also join the two arrays
+
+
+// console.log(Array.isArray("kunal"))// asking that the given value is array or not 
+// console.log(Array.from("kunal"));// make array by value which is given to you 
+// console.log(Array.from({name: "kunal"})); // interesting
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+
+// console.log(Array.of(score1,score2,score3));// it also make array from the given three arrays
