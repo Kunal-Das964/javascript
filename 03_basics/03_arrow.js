9c274ac125d9a1aa9b89616e0844e2389bfd2d5f
Kunal Das <wastematerial12345@gmail.com>
2024-10-08 20:05:12 +0000

    arrow function done

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,49 @@
+const user = {
+    username:"kunal",
+    price:999,
+
+
+    welcomeMessage: function (){
+        console.log(`${this.username},welcome to website`)
+        console.log(this);
+        
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+// console.log(this);
+
+// function chai(){
+//     let username = "kunal"
+//     console.log(this);
+    
+// }
+
+// chai()
+
+// const chai = () => {
+//     let username = "kunal"
+//     console.log(this);
+// }
+// chai()
+// arrow function
+
+
+// const addTwo = (num1,num2) => {
+//     return num1 + num2
+// }
+// const addTwo = (num1,num2) => num1 + num2
+
+// const addTwo = (num1,num2) => ( num1 + num2 )
+
+const addTwo = (num1,num2) =>( {username: "kunal"} )
+
+console.log(addTwo(3,4))
+
+// const myArray = [2,5,3,7,8]
+
+// myArray.forEach()
+
