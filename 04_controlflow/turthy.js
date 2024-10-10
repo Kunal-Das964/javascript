056f687d2ffc3b1c9f9b7eb66654d7f6233e6de0
Kunal Das <wastematerial12345@gmail.com>
2024-10-10 10:16:52 +0000

    control flow done

---
+++ 04_controlflow/turthy.js
@@ -0,0 +1,43 @@
+// const userEmail = []
+
+// if (userEmail) {
+//     console.log("Got user email");
+// }else{
+//     console.log("don't have user email");
+// }
+
+// falsy value : false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//turthy value : "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("array is empty");
+// }
+
+// const emptyobj = {}
+
+// if (Object.keys(emptyobj).length === 0) {
+//     console.log("Object is empty");
+    
+// }
+
+// nullish Coalescing Operator (??): null undefined
+// this is callback
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+val1 = undefined ?? 10 ?? 20
+
+
+console.log(val1);
+
+
+// Terniary Opertor 
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
